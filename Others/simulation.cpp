96d58831c5329271e791e60646b640f8de0a9704
xuyang-xy <xuyang_yxu@126.com>
2021-03-13 03:21:18 +0800

    update

--- Others/simulation.cpp
+++ Others/simulation.cpp
@@ -281,3 +281,30 @@ void nextPermutation(vector<int>& nums) {
     }
     reverse(nums.begin() + l + 1, nums.end());
 }
+
+string decodeString(string s) {
+    string res = "";
+    int len = s.length();
+    stack<int> nums;
+    stack<string> strs;
+    int tempn = 0;
+    for (int i = 0; i < len; i++) {
+        if (s[i] >= '0' && s[i] <= '9') tempn = tempn * 10 + int(s[i] - '0');
+        else if (s[i] == '[') {
+            nums.push(tempn);
+            strs.push(res);
+            tempn = 0;
+            res = "";
+        }
+        else if (s[i] == ']') {
+            int n = nums.top();
+            nums.pop();
+            string temps = strs.top();
+            strs.pop();
+            for (int j = 0; j < n; j++) temps += res;
+            res = temps;
+        }
+        else res += s[i];
+    }
+    return res;
+}
