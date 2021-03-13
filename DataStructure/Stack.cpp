ec33a22da2ee6bd258727f494615b42e59e2755f
xuyang-xy <xuyang_yxu@126.com>
2021-03-13 23:38:06 +0800

    update

--- DataStructure/Stack.cpp
+++ DataStructure/Stack.cpp
@@ -1,26 +1,18 @@
-string decodeString(string s) {
-    string res = "";
+bool isValid(string s) {
     int len = s.length();
-    stack<int> nums;
-    stack<string> strs;
-    int tempn = 0;
+    if (len == 0) return true;
+    map<char, char> pairs{
+        {'(', ')'},
+        {'[', ']'},
+        {'{', '}'}
+    };
+    stack<char> lefts;
     for (int i = 0; i < len; i++) {
-        if (s[i] >= '0' && s[i] <= '9') tempn = tempn * 10 + int(s[i] - '0');
-        else if (s[i] == '[') {
-            nums.push(tempn);
-            strs.push(res);
-            tempn = 0;
-            res = "";
+        if (pairs.count(s[i])) lefts.push(s[i]);
+        else {
+            if (lefts.empty() || s[i] != pairs[lefts.top()]) return false;
+            else lefts.pop();
         }
-        else if (s[i] == ']') {
-            int n = nums.top();
-            nums.pop();
-            string temps = strs.top();
-            strs.pop();
-            for (int j = 0; j < n; j++) temps += res;
-            res = temps;
-        }
-        else res += s[i];
     }
-    return res;
+    return lefts.size() == 0;
 }
