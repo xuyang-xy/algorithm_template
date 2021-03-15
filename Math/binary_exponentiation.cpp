f677731e3f914233ead90e79e87f24361ae2b64a
xuyang-xy <xuyang_yxu@126.com>
2021-03-16 02:29:48 +0800

    update

--- Math/binary_exponentiation.cpp
+++ Math/binary_exponentiation.cpp
@@ -7,3 +7,13 @@ double PowerPow(double a, long long b) {
     }
     return res;
 }
+
+int PowerPow(long long a, long long b, long long m) {
+    long long res = 1;
+    while (b) {
+        if (b & 1) res = (res * a) % m;
+        a = (a * a) % m;
+        b >>= 1;
+    }
+    return res;
+}
