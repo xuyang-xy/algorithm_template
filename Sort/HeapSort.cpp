5318f40be268ed5b928dcd1e77d1be09e499fbca
xxyangxu <xxyangxu@tencent.com>
2022-06-21 14:05:39 +0800

    [add HeapSort] 20220621

--- Sort/HeapSort.cpp
+++ Sort/HeapSort.cpp
@@ -26,4 +26,16 @@ void HeapSort(vector<int>& nums) {
 }
 
 // 模板题
-/*topk， 数据流中位数*/
+/*
+topk
+数据流中位数
+*/
+// leetcode 215 数组第k大
+int findKthLargest(vector<int>& nums, int k) {
+    // priority_queue<int> maxheap;
+    // for (int i = 0; i < nums.size(); i++)
+    //     maxheap.push(nums[i]);
+    // for (int i = 1; i < k; i++)
+    //     maxheap.pop();
+    // return maxheap.top();
+}
---
+++ Sort/HeapSort.md
@@ -0,0 +1,5 @@
+# 堆排相关
+//升序队列
+priority_queue <int,vector<int>,greater<int> > q;
+//降序队列
+priority_queue <int,vector<int>,less<int> >q;
